29386665031051267b48109ab004795078311488
ostende <32985634+ostende@users.noreply.github.com>
2020-04-14 01:42:47 +0200

    Update HardwareInfo.py

--- lib/python/Tools/HardwareInfo.py
+++ lib/python/Tools/HardwareInfo.py
@@ -37,7 +37,7 @@ class HardwareInfo:
 
 		# Model
 		try:
-			self.device_model = open("/proc/stb/info/gbmodel").read().strip()
+			self.device_model = open("/proc/stb/info/vumodel").read().strip()
 		except:
 			pass
 
@@ -45,7 +45,7 @@ class HardwareInfo:
 			self.device_model = self.device_name
 
 		# HDMI capbility
-		if getMachineBuild() in ('gb7325', 'gb7358', 'gb7356', 'gb7362', 'gb73625', 'gb72525', 'gb7252', 'gb72604', 'xc7362', 'hd2400', 'hd51', 'gbmv200', 'gb72604'):
+		if getMachineBuild() in ('vusolo2', 'vuzero4k', 'gb7325', 'gb7358', 'gb7356', 'gb7362', 'gb73625', 'gb72525', 'gb7252', 'gb72604', 'xc7362', 'hd2400', 'hd51', 'gbmv200', 'gb72604'):
 			self.device_hdmi = True
 		else:
 			self.device_hdmi = False
